af2f8c09f5ec4f8964410aec08fbbe3e392bd161
bohlinger <patrikb@met.no>
2019-10-23 07:30:52 +0000

    quick fix: copernicus path added to avoid error due to dependencies

--- wavyMini/pathfinder.py
+++ wavyMini/pathfinder.py
@@ -4,6 +4,7 @@ python file including needed paths
 
 downloadpath = '/home/vietadm/wavyMini/data/altimetry/'
 
+satpath_copernicus = '/lustre/storeB/project/copernicus/ARC-MFC/sentinel3a/'
 satpath_ftp_014_001 = ('/Core/'
                     + 'WAVE_GLO_WAV_L3_SWH_NRT_OBSERVATIONS_014_001/'
                     + 'dataset-wav-alti-l3-swh-rt-global-'
